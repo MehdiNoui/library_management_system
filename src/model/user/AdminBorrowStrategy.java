29ca906db17fcd5638402a8ca66bf5fe0ba68692
MehdiNoui <mehdinoui2004@gmail.com>
2025-04-27 05:46:36 +0100

    updated admin borrow strategy

--- src/model/user/AdminBorrowStrategy.java
+++ src/model/user/AdminBorrowStrategy.java
@@ -5,10 +5,6 @@ import model.Book;
 public class AdminBorrowStrategy implements BorrowStrategy {
     @Override
     public boolean borrow(User user, Book book) {
-        if (book.getAvailableBooks() > 0) {
-            book.setAvailableBooks(book.getAvailableBooks() - 1);
-            return true;
-        }
         return false;
     }
 }
